4a9ca3781fabfb25cc93e332b3ab84081ffac8ba
Utsav Raithatha <utsav.r@ahduni.edu.in>
2023-12-10 21:42:11 +0530

    Modify the layout

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Image
+        height={45}
+        width={45}
+        alt="logo"
+        src="/logo.svg"
+      />
+      <p style={{ marginLeft: '10px' }}>EduVerse</p>
+    </div>
   )
 }
